7dd39520c1716b876691d4d1fefd0defcff98a05
Tristan White <oliver.tristan@gmail.com>
2019-06-21 12:29:18 +0200

    Add bitcoin link

--- _inc/footer.php
+++ _inc/footer.php
@@ -2,7 +2,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col text-center">
-                        &copy; <?php echo date("Y"); ?> <a href="https://tristanwhite.info" target="_blank">Tristan White</a>
+                        &copy; <?php echo date("Y"); ?> <a href="https://tristanwhite.info" target="_blank">Tristan White</a><br>
+                        BTC: <a href="bitcoin:3F15Wh3nyhsnze5Gg5FkQXyDhWEr4bh8sH">3F15Wh3nyhsnze5Gg5FkQXyDhWEr4bh8sH</a>
                     </div>
                 </div>
             </div>
