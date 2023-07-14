245c8c85580d90aee3ff338b70550b42da41de1a
Tristan White <oliver.tristan@gmail.com>
2023-07-14 09:02:51 +0200

    Update footer

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,6 +1,6 @@
 <footer id="footer">
 	<p>Designed, built, and maintained by <a href="https://triss.dev" target="_blank">Tristan White</a> © <?php echo date("Y"); ?>.</p>
-	<p>Suggest a tool or report a bug: <a href="https://github.com/triss90/webbox.dev/issues" target="_blank">GitHub Issues</a>.</p>
+	<p>Suggest a tool or report a bug: <a href="https://github.com/triss90/webbox.dev/issues/new/choose" target="_blank">GitHub Issues</a>.</p>
 </footer>				
 <script src="/assets/js/app.js"></script>
 <script src="/assets/js/clipboard.js"></script>
