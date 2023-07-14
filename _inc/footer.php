9953aa451cc7fa2169daa98d6dd455fc1eeb34d8
Tristan White <oliver.tristan@gmail.com>
2023-07-14 08:38:44 +0200

    Add bugreport to footer

--- _inc/footer.php
+++ _inc/footer.php
@@ -1,5 +1,6 @@
 <footer id="footer">
-	<p>Designed, built, and maintained by <a href="https://triss.dev" target="_blank">Tristan White</a> © <?php echo date("Y"); ?></p>
+	<p>Designed, built, and maintained by <a href="https://triss.dev" target="_blank">Tristan White</a> © <?php echo date("Y"); ?>.</p>
+	<p>Suggest a tool or report a bug: <a href="https://github.com/triss90/webbox.dev/issues" target="_blank">GitHub Issues</a>.</p>
 </footer>				
 <script src="/assets/js/app.js"></script>
 <script src="/assets/js/clipboard.js"></script>
